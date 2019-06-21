ab11f9acf22eb458cf569e79bcfcc92053a85251
Rishita Hariyani <rhariyani@cnm.edu>
2019-06-21 11:08:58 -0600

    4th assignment phase2

---
+++ objects and apis.js
@@ -0,0 +1,28 @@
+/*
+* Objects, Interfaces, and API's
+ */
+/*
+ let dog ={
+ 	name: "Steve",
+	 color: "brown",
+	 breed: "pug",
+	 size: "small",
+	 bark: function(typeOfBark){
+ 		console.log("Bark!");
+	 },
+ };
+ dog.bark();
+
+ */
+
+function x(y)
+{
+	y.num =  y.num + 5;
+	console.log(y);
+}
+let y = {
+	name:"Tommy",
+	num: 10,
+};
+x(y);
+console.log(y);
